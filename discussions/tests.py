8960f7578ee5791dbacbae1b9cf7bda560a1cd49
Jukka-Pekka <jpkerkkanen@gmail.com>
2020-04-28 00:22:42 +0200

    Learning Django with Vitor: part 3 done

--- discussions/tests.py
+++ discussions/tests.py
@@ -1,8 +1,10 @@
 from django.urls import reverse
 from django.urls import resolve
 from django.test import TestCase
+from django.contrib.auth.models import User
 from .views import home, discussion_topics, new_topic #metodinkin voi näin tuoda.
-from .models import Discussion
+from .models import Discussion, Topic, Post
+from .forms import NewTopicForm
 
 # Create your tests here.
 class HomeTests(TestCase):
@@ -42,17 +44,20 @@ class DiscussionTopicsTests(TestCase):
         view = resolve('/discussions/1/')
         self.assertEquals(view.func, discussion_topics)
 
-    def test_discussion_topics_view_contains_link_to_home_page(self):
+    def test_discussion_topics_view_contains_links(self):
         topics_url = reverse('url_discussion_topics', kwargs={'discussion_id':1})
         response = self.client.get(topics_url)
         home_url = reverse('url_home')
+        new_topic_url = reverse('url_new_topic', kwargs={'discussion_id':1})
 
         #{0} refers to the 1st param of format function.
         self.assertContains(response, 'href="{0}"'.format(home_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
 
 class NewTopicsTests(TestCase):
     def setUp(self):
         Discussion.objects.create(name='Django', description='Django discussion')
+        User.objects.create_user(username='john', email='john@doe.com', password='123')
 
     def test_new_topic_view_success_status_code(self):
         url = reverse('url_new_topic', kwargs={'discussion_id': 1})
@@ -68,18 +73,68 @@ class NewTopicsTests(TestCase):
         view = resolve('/discussions/1/new/')
         self.assertEquals(view.func, new_topic)
 
-    def test_new_topic_view_contains_link_to_home_page(self):
+    def test_new_topic_view_contains_links(self):
         url = reverse('url_new_topic', kwargs={'discussion_id':1})
         response = self.client.get(url)
         home_url = reverse('url_home')
-
-        #{0} refers to the 1st param of format function.
-        self.assertContains(response, 'href="{0}"'.format(home_url))
-
-    def test_new_topic_view_contains_link_to_discussion(self):
-        url = reverse('url_new_topic', kwargs={'discussion_id':1})
-        response = self.client.get(url)
         topics_url = reverse('url_discussion_topics', kwargs={'discussion_id':1})
 
-        #{0} refers to the 1st param of format function.
+        # {0} refers to the 1st param of format function.
+        self.assertContains(response, 'href="{0}"'.format(home_url))
         self.assertContains(response, 'href="{0}"'.format(topics_url))
+
+    def test_csrf(self):
+        url = reverse('url_new_topic', kwargs={'discussion_id': 1})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_new_topic_valid_post_data(self):
+        url = reverse('url_new_topic', kwargs={'discussion_id': 1})
+        data = {
+            'subject': 'Test title',
+            'message': 'Lorem ipsum dolor sit amet'
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
+    def test_new_topic_invalid_post_data(self):
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('url_new_topic', kwargs={'discussion_id': 1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_invalid_post_data_empty_fields(self):
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('url_new_topic', kwargs={'discussion_id': 1})
+        data = {
+            'subject': '',
+            'message': ''
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Post.objects.exists())
+
+    def test_contains_form(self):  # <- new test
+        url = reverse('url_new_topic', kwargs={'discussion_id': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+
+    def test_new_topic_invalid_post_data(self):  # <- updated this one
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('url_new_topic', kwargs={'discussion_id': 1})
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
--- discussions/views.py
+++ discussions/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.http import Http404
 from .models import Discussion, User, Topic, Post
+from .forms import NewTopicForm
 # Create your views here.
 def home(request):
     discussions = Discussion.objects.all()
@@ -15,25 +16,22 @@ def discussion_topics(request, discussion_id):
 # Note: this both shows a new topic form and saves a new topic!
 def new_topic(request, discussion_id):
     disc = get_object_or_404 (Discussion, pk=discussion_id)
+    user = User.objects.first()
 
+    # POST -> user is submitting date to the server.
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first()  # TODO: get the currently logged in user
-
-        topic = Topic.objects.create(
-            subject=subject,
-            discussion=disc,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        return redirect('url_discussion_topics', discussion_id=disc.pk)
-
-    return render(request, 'new_topic.html', {'discussion': disc})
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.discussion = disc
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('url_discussion_topics', discussion_id=disc.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'discussion': disc, 'form':form})
